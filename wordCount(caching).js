139dc66a7ff2257571e512b316974c4ad885bd34
ngabo <ngaboizo2000@gmail.com>
2025-08-05 23:23:54 +0200

    function that counts the number of words in a sentence and implements caching

---
+++ wordCount(caching).js
@@ -0,0 +1,18 @@
+function wordCount(str){
+  if(typeof str !== 'string') throw new Error('str is not a string');
+
+
+  wordCount.cache = wordCount.cache || {};
+  let a = str.split(' ');
+  let key = a.join(' ');
+
+  if(wordCount.cache[key]) return wordCount.cache[key];
+ 
+  wordCount.cache[key] = a.length;
+  return wordCount.cache[key]
+  
+
+}
+console.log(wordCount("I love JavaScript"));
+console.log(wordCount("   multiple   spaces  "));// length is not 9
+console.log(wordCount("Closures are power"));
