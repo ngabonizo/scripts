4499d4333df5a6f169c689627192325f7ae0848c
ngabo <ngaboizo2000@gmail.com>
2025-08-21 05:41:39 +0200

    sorting odds only leaving evens in place

--- sortOddOnly.js
+++ sortOddOnly.js
@@ -1,10 +1,24 @@
+//Sort the odd numbers in ascending order while leaving the even numbers at their original positions
+
+
+/*
 function sortArray(array) {
   if(Object.prototype.toString.call(array).slice(8,-1) !== 'Array')
     throw new Error('argument not an array');
   
+  
  return array.sort((a,b) => {
-    if(a % 2 !==0 && b % 2 !== 0) return a-b;
+    if(a % 2 !==0 && b % 2 !== 0){
+  return a - b;
+    } 
   })
+}
+  */
+
+
+function sortArray(array) {
+  var odd = array.filter(elem => elem % 2 !== 0).sort((a, b) => a - b);
+  return array.map(elem => elem % 2 === 0 ? elem : odd.shift());
 }
 
 
@@ -14,3 +28,6 @@ let a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]; //  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 console.log(sortArray(a));
 console.log(sortArray(b));
 console.log(sortArray(c));
+
+
+// study this code
