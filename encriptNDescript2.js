8697c92c44bbcb9e61c03357e2e7d4112a6e2b86
ngabo <ngaboizo2000@gmail.com>
2025-09-15 12:30:02 +0200

    incript and decripts functions

---
+++ encriptNDescript2.js
@@ -0,0 +1,7 @@
+function encrypt(text, n=0) {
+  return n<1 || text==null ? text : encrypt( [...text].reduce(([a,b],v,i)=> i%2 ? [a+v,b]:[a,b+v],['','']).join(''), n-1)
+}
+
+function decrypt(text, n) {
+  return n<1 || text==null ? text : decrypt( [...text].slice(text.length/2).map((v,i) => v+text[i] ).join('').slice(0,text.length), n-1)
+}
