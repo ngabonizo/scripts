ed63e62241d295cbd9e0cb81fbfdc4e0ee6823f8
ngabo <ngaboizo2000@gmail.com>
2025-11-01 15:18:52 +0200

    array shuffling function

--- shuffle2.js
+++ shuffle2.js
@@ -1,7 +1,17 @@
+// function shuffleArray(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     // Pick a random index from 0 to i
+//     const j = Math.floor(Math.random() * (i + 1));
+//     // Swap elements at indices i and j
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// }
+
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = array.length - 1; i > 0; i--) { // i>=0 not needed cuz last first value doesn't need to be shuffled
     // Pick a random index from 0 to i
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(Math.random() * (i+1));
     // Swap elements at indices i and j
     [array[i], array[j]] = [array[j], array[i]];
   }
